213eca5f174c4466f236ac59848df77ecbcc59da
Nitij <nitij.jp@gmail.com>
2013-12-07 21:55:57 +0530

    Added country grid.

---
+++ Script/sampleCountryData.js
@@ -0,0 +1,17 @@
+;
+(function (w) {
+    var countryDataSource =
+        [
+            { name: "China", capital: "Beijing", area: "9,706,961", population: "1,350,695,000", flag: "188px-Flag_of_the_People's_Republic_of_China.svg.png" },
+            { name: "India", capital: "New Delhi", area: "3,287,590", population: "1,210,193,422", flag: "188px-Flag_of_India.svg.png" },
+            { name: "United States of America", capital: "Washington, D.C.", area: "9,826,675", population: "317,208,000", flag: "188px-Flag_of_the_United_States.svg.png" },
+            { name: "Indonesia", capital: "Jakarta", area: "1,904,569", population: "237,424,363", flag: "188px-Flag_of_Indonesia.svg.png" },
+            { name: "Brazil", capital: "Brasília", area: "8,515,767", population: "201,032,714", flag: "188px-Flag_of_Brazil.svg.png" },
+            { name: "Pakistan", capital: "Islamabad", area: "796,095", population: "182,490,72", flag: "188px-Flag_of_Pakistan.svg.png" },
+            { name: "Nigeria", capital: "Abuja", area: "923,768", population: "174,507,539", flag: "188px-Flag_of_Nigeria.svg.png" },
+            { name: "Bangladesh", capital: "Dhaka", area: "147,570", population: "150,039,000", flag: "188px-Flag_of_Bangladesh.svg.png" },
+            { name: "Russia", capital: "Moscow", area: "17,098,242", population: "143,600,000", flag: "188px-Flag_of_Russia.svg.png" },
+            { name: "Japan", capital: "Tokyo", area: "377,944", population: "126,659,683", flag: "188px-Flag_of_Japan.svg.png" }
+        ];
+    w["CountryDataSource"] = countryDataSource;
+})(window);

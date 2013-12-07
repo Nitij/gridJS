25aea13cd2e8d78227c894aa1e01bcb12ab88bba
Nitij <nitij.jp@gmail.com>
2013-12-07 14:30:06 +0530

    Updated input binding logic.
    Updated input binding code logic, also now whenever any input is changed
    entire row is re-binded.

---
+++ sampleGeneralData.js
@@ -0,0 +1,49 @@
+;
+(function (w) {
+    var generalDataSource =
+        [
+            {
+                sampleCheckValue: true,
+                sampleText: "Jason Statham",
+                sampleNumber: 1,
+                sampleRadio: false
+            }
+            , {
+                sampleCheckValue: true,
+                sampleText: "Jason Statham",
+                sampleNumber: 1,
+                sampleRadio: false
+            }
+            , {
+                sampleCheckValue: true,
+                sampleText: "Jason Statham",
+                sampleNumber: 1,
+                sampleRadio: false
+            }
+            , {
+                sampleCheckValue: false,
+                sampleText: "Jason Statham",
+                sampleNumber: 1,
+                sampleRadio: false
+            }
+            , {
+                sampleCheckValue: true,
+                sampleText: "Jason Statham",
+                sampleNumber: 1,
+                sampleRadio: false
+            }
+            , {
+                sampleCheckValue: true,
+                sampleText: "Jason Statham",
+                sampleNumber: 1,
+                sampleRadio: false
+            }
+            , {
+                sampleCheckValue: true,
+                sampleText: "Jason Statham",
+                sampleNumber: 1,
+                sampleRadio: false
+            }
+        ];
+    w["GeneralDataSource"] = generalDataSource;
+})(window);
